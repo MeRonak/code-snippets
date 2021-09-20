42522195b18425cc6dfc358d0b4f669ccdc073ce
MeRonak <87123160+MeRonak@users.noreply.github.com>
2021-09-20 14:30:21 +0530

    Update sieves_theorem.cpp

--- sieves_theorem.cpp
+++ sieves_theorem.cpp
@@ -20,3 +20,24 @@ int main(){
 return 0;}
 //sieve theorem to get all the prime number upto a given number in the
 //time complexity of O(NloglogN).
+
+*****************************************************************updated sieves code*************************************************************
+ #include<bits/stdc++.h>
+ using namespace std;
+
+void isprime(){
+    cout<<'2'<<endl;
+    vector<bool> v(1e6,true);
+    for(int i = 3 ;i<(int)1e6+1;++i){
+        if(v[i]){
+            cout<<i<<endl;
+            for(int j = 1 ; j<=1e6 ; j+=i){
+                v[i] = false;
+            }
+        }
+    }
+}
+
+int main(){
+    isprime();
+return 0;}
